d72e0aea8aaaf36505ebfd20b8e6571e670ac779
Peter <psquicciarini@redventures.com>
2017-06-01 17:08:18 -0400

    Adds Windows functionality

--- index.js
+++ index.js
@@ -5,14 +5,27 @@
 const express = require('express');
 
 const mbUrl = 'https://gist.githubusercontent.com/stripedpajamas/b8f9b651fa91dfef5d8cf887598bd097/raw/324025cbd26f83b3b2aa84c461b9c9e38489b5ee/mobydick.txt';
-const data = `#!/bin/bash
-# run bash <(curl -s [url]) to do it
+
+const osxData = `#!/bin/bash
+# run sh <(curl -sL [url]) to do it
 curl -s ${mbUrl} | say`;
+
+const wData = `# run . { iwr [url] } | iex
+   $md = iwr ${mbUrl}
+   Add-Type -AssemblyName System.Speech
+   $synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+   $synth.Speak($md)`;
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.type('text');
-  res.send(data);
+  res.send(osxData);
 });
+
+app.get('/w', (req, res) => {
+  res.type('text');
+  res.send(wData)
+});
+
 app.listen(process.env.PORT || 3000);
